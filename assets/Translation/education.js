a321c0e750d2157fd99bded917be26b3792117b8
Saja <91623920+Watch-Me-Fly@users.noreply.github.com>
2022-06-10 15:08:07 +0200

    translations

---
+++ assets/Translation/education.js
@@ -0,0 +1,64 @@
+const education = {
+
+    // ANCHOR : english
+    en: {
+        // DWWM
+        education: "Education",
+        dwwm: "Professional certification in Web Development and Mobile Web",
+        dwwmWhen: "Mar 2022 - Nov 2022",
+        dwwmWhere: "France",
+        dwwmWhat1: "Full Stack",
+        dwwmWhat2: "RNCP level V (equivalent to BAC+2 in France)",
+        // autodidicate
+        frontEnd: "Front end web development in autodidacte",
+        frontEndWhen: "Sep 2021 - Mar 2022",
+        frontEndWhere: "Coursera.org + OpenClassrooms",
+        general: "General",
+        frontEndWhatLi1: "Understand the web",
+        frontEndWhatLi2: "",
+        frontEndWhatLi3: "create websites using HTML5 and CSS",
+        frontEndWhatLi4: "Command line in terminal",
+        frontEndWhatLi5: "Algorithms",
+        frontEndWhatLi6: "HTML, CSS, Bootstrap, JS, basics of Ajax",
+        frontEndWhatLi7: "Basics of JS",
+        // waitress
+        Waitress: "Waitress",
+        WaitressWhen: "Aug 2021 - Dec 2021",
+        WaitressWhere: "Hôtel, France",
+        // gastro intern
+        gastroResi: "Service resident doctor",
+        gastroResiWhen: "May 2021 - Jul 2021",
+        gastroResiWhere: "Hospital, France",
+        gastroResiService: "Gastroenterologie Service",
+        gastroResiWhat: "Participate in diagnosing, treating, and practicing minor intervention under the responsibility of the chief of department.",
+        // emr Intern
+        emrResi: "Service resident doctor",
+        emrResiWhen: "Nov 2020 - Apr 2021",
+        emrResiWhere: "Hospital, France",
+        emrResiService: "Emergency Medicine for Adults and COVID unit",
+        emrResiWhat: "articipate in diagnosing, treating, and practicing minor intervention under the        responsibility of the chief of department.",
+        // med intern ksa
+        medInt: "Medical Intern",
+        medIntWhen: "Jul 2017 - Jun 2018",
+        medIntWhere: "Multiple hospitals - Riyadh, Saudi Arabia",
+        medIntWhat: "Observation, practice of minor procedures under the responsability of the chief of service.",
+        // translator
+        translator: "Translator Ar / En",
+        translatorWhen: "May 2012 - Jan 2016",
+        translatorWhere: "Emdad Najd - Riyadh, Saudi Arabia",
+        translatorWhat: "Translation of technical and industrial documents from one language to the other.",
+        // ps teacher:
+        psTeacher: "Photoshop Teacher",
+        psTeacherWhen: "1 semester, 2009 - 2010",
+        psTeacherWhere: "65th High School for girls - Riyadh, Saudi Arabia",
+        psTeacherWhat: "Teaching photoshop to students and teachers for a semester as a volunteer",
+    },
+    // ANCHOR : french
+    fr: {
+
+    },
+    // ANCHOR : arabic
+    ar: {
+
+    }
+}
---
+++ assets/Translation/experience.js
@@ -0,0 +1,60 @@
+const experience = {
+
+    // ANCHOR : english
+    en: {
+        // WebDev Intern
+        experience: "Experience",
+        WebDevInt: "Intern - Web Developer",
+        WebDevIntWhen: "Jun 2022 - Sep 2022",
+        WebDevIntWhere: "OVRSEA - France",
+        WebDevIntWhat1: "Team working",
+        WebDevIntWhat2: "Coding, code-review, documentation",
+        WebDevIntWhat3: "Preparing a project",
+        // Sistech
+        SistechExp: "Sistech Fellow",
+        SistechExpWhen: "Sep 2021 - Mar 2022",
+        SistechExpWhere: "TechFugees, France",
+        SistechExpWhat: "Professional reorintation towards programming. With this program:",
+        SistechExpWhatLi1: "I followed onling courses",
+        SistechExpWhatLi2: "had a mentor, a teacher, and a linguistic tandem",
+        // waitress
+        Waitress: "Waitress",
+        WaitressWhen: "Aug 2021 - Dec 2021",
+        WaitressWhere: "Hôtel, France",
+        // gastro intern
+        gastroResi: "Service resident doctor",
+        gastroResiWhen: "May 2021 - Jul 2021",
+        gastroResiWhere: "Hospital, France",
+        gastroResiService: "Gastroenterologie Service",
+        gastroResiWhat: "Participate in diagnosing, treating, and practicing minor intervention under the responsibility of the chief of department.",
+        // emr Intern
+        emrResi: "Service resident doctor",
+        emrResiWhen: "Nov 2020 - Apr 2021",
+        emrResiWhere: "Hospital, France",
+        emrResiService: "Emergency Medicine for Adults and COVID unit",
+        emrResiWhat: "articipate in diagnosing, treating, and practicing minor intervention under the        responsibility of the chief of department.",
+        // med intern ksa
+        medInt: "Medical Intern",
+        medIntWhen: "Jul 2017 - Jun 2018",
+        medIntWhere: "Multiple hospitals - Riyadh, Saudi Arabia",
+        medIntWhat: "Observation, practice of minor procedures under the responsability of the chief of service.",
+        // translator
+        translator: "Translator Ar / En",
+        translatorWhen: "May 2012 - Jan 2016",
+        translatorWhere: "Emdad Najd - Riyadh, Saudi Arabia",
+        translatorWhat: "Translation of technical and industrial documents from one language to the other.",
+        // ps teacher:
+        psTeacher: "Photoshop Teacher",
+        psTeacherWhen: "1 semester, 2009 - 2010",
+        psTeacherWhere: "65th High School for girls - Riyadh, Saudi Arabia",
+        psTeacherWhat: "Teaching photoshop to students and teachers for a semester as a volunteer",
+    },
+    // ANCHOR : french
+    fr: {
+
+    },
+    // ANCHOR : arabic
+    ar: {
+
+    }
+}
